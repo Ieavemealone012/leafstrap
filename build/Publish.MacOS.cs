ea3deeeaa79d6d2b7794d6759e69a1c6b55a660f
Invra <identificationsucks@gmail.com>
2026-09-14 04:36:35 +1000

    chore: xcode .app building implemented

--- build/Publish.MacOS.cs
+++ build/Publish.MacOS.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics;
 using System.IO;
 using Fallout.Common;
 using Fallout.Common.IO;
@@ -7,7 +8,9 @@ public partial class Build : FalloutBuild
 {
     void PublishMacOS(string outputDirectory)
 	{
-        AbsolutePath virtualbackendBuildRoot = $"{GitRoot}/backend/virtualdisplay/.build";
+        AbsolutePath virtualbackendBuildRoot = GitRoot / "backend" / "virtualdisplay" / ".build";
+        AbsolutePath macAppLocation = GitRoot / "packaging" / "macApp";
+        AbsolutePath xcodeProjectLocation = macAppLocation / "macApp.xcodeproj";
 
         if (!File.Exists((AbsolutePath)outputDirectory / "libvirtualdisplay.dylib")
             && File.Exists($"{virtualbackendBuildRoot}/out/Products/Release/libvirtualdisplay.dylib"))
@@ -21,6 +24,32 @@ public partial class Build : FalloutBuild
             Log.Information("Copying over {Source} into {OutDir}", source, OutputRoot);
             File.Copy(source, (AbsolutePath)outputDirectory / "libvirtualdisplay.dylib");
         }
+
+        Log.Information("Building {xcproj} with xcodebuild", xcodeProjectLocation);
+        var xcbProc = new Process();
+        xcbProc.StartInfo.FileName = "xcodebuild";
+
+        xcbProc.StartInfo.Arguments = $"-project {xcodeProjectLocation} " +
+                                      "-target Froststrap " +
+                                      $"-configuration {Configuration} " +
+                                      "build";
+
+        xcbProc.StartInfo.UseShellExecute = false;
+
+        xcbProc.Start();
+        xcbProc.WaitForExit();
+
+        var src = (AbsolutePath)macAppLocation / "build" / Configuration / "Froststrap.app";
+        var dest = (AbsolutePath)outputDirectory / "Froststrap.app";
+        Log.Information("Copying {src} artifact to {OutDir}", src, dest);
+        
+        var copyProc = new Process();
+        copyProc.StartInfo.FileName = "cp";
+        copyProc.StartInfo.Arguments = $"-r \"{(string)src}\" \"{(string)dest}\"";
+        copyProc.StartInfo.UseShellExecute = false;
+
+        copyProc.Start();
+        copyProc.WaitForExit();
 	}
 }
 
