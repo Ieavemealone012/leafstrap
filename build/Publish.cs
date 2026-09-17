6568b7e778ed6c535791649bdeeba76e3440d9a3
Invra <identificationsucks@gmail.com>
2026-09-17 20:03:14 +1000

    that would be hepful

--- build/Publish.cs
+++ build/Publish.cs
@@ -70,6 +70,7 @@ public partial class Build : FalloutBuild
 
         if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX)) PublishMacOS(outputDirectory);
         if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows)) PublishWindows(outputDirectory);
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux)) PublishLinux(outputDirectory);
 
         if (process.ExitCode != 0)
         {
