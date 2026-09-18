9b9b48c9342b98f9f9fcac91aa4005ca4b78e1d8
Invra <identificationsucks@gmail.com>
2026-09-19 01:30:32 +1000

    chore: remove useless nuget config

--- build/Publish/Publish.cs
+++ build/Publish/Publish.cs
@@ -50,7 +50,6 @@ public partial class Build : FalloutBuild
                                       $"-c {Configuration} " +
                                       $"-r {rid} " +
                                       $"-o \"{outputDirectory}\" " +
-                                      // $"--configfile \"{GitRoot}/nuget.config\" " +
                                       $"-p:PublishProfile=\"{publishProfile}\" " +
                                       $"--nologo";
 
